a1ff1cd59b5a45805d2b9c1a01a176aa33711169
zsh19 <zsh1980794141@126.com>
2022-12-24 21:15:38 +0800

    修改请求路径

--- blue_prints/detect_api.py
+++ blue_prints/detect_api.py
@@ -18,26 +18,17 @@ api = Api(detect_api, version='1.0', title='crystal detection API',
 ns = api.namespace('detect_api', description='结晶检测相关接口')
 
 
-@ns.route('/start_detect/')
-# 需要先处理图片为 640, 图片路径请传入绝对路径
+@ns.route('/start_detect/<path:img_path>')
 class DETECT(Resource):
-    def get(img_path='not specified'):
-        """
-            输入一张结晶图片, 返回
-            :param img_path:
-            :return:
-        """
-        # 传入参数，path绝对路径
-        source = request.args.get("path")
-        # 如果图片来源于网络，那么就把图片下载下来
-        if "http" in source:
-            req = requests.get(source)
-            fileName = str(source).split("/")[-1]
+    def get(self, img_path='not specified'):
+        if "http" in img_path:
+            req = requests.get(img_path)
+            fileName = str(img_path).split("/")[-1]
             source = os.path.split(os.path.realpath(__file__))[0] + '/detect/inference/images/' + fileName
             with open(source, 'wb') as f:
                 f.write(req.content)
         else:
-            source = source
+            source = img_path
 
         result = detect(
             source=source,
@@ -49,47 +40,12 @@ class DETECT(Resource):
             augment=False,
         )
         # view_image(image_path=source, result=result)
-        # 结果可能还需要处理为整数
         response = make_response(jsonify(
-            {"description": "result中的元素的第一个位置是类别，剩下四个是x1y1x2y2", "code": 200, "result": result}
+            {"description": "result中的元素的第一个位置是类别，剩下四个是x1y1x2y2", "code": 2000, "result": result}
         ))
-
-        # 检测完成之后，将图片删除
         if os.path.exists(source):
             os.remove(source)
             print('成功删除文件:', source)
         else:
             print('未找到此文件:', source)
         return response
-
-# @ns.route('/start_detect/<path:img_path>')
-# class DETECT(Resource):
-#     def get(self, img_path='not specified'):
-#         if "http" in img_path:
-#             req = requests.get(img_path)
-#             fileName = str(img_path).split("/")[-1]
-#             source = os.path.split(os.path.realpath(__file__))[0] + '/detect/inference/images/' + fileName
-#             with open(source, 'wb') as f:
-#                 f.write(req.content)
-#         else:
-#             source = img_path
-#
-#         result = detect(
-#             source=source,
-#             weights=os.path.split(os.path.realpath(__file__))[0] + "/detect/best.pt",
-#             img_size=640,
-#             conf_thres=0.25,
-#             iou_thres=0.1,
-#             save_conf=False,
-#             augment=False,
-#         )
-#         # view_image(image_path=source, result=result)
-#         response = make_response(jsonify(
-#             {"description": "result中的元素的第一个位置是类别，剩下四个是x1y1x2y2", "code": 2000, "result": result}
-#         ))
-#         if os.path.exists(source):
-#             os.remove(source)
-#             print('成功删除文件:', source)
-#         else:
-#             print('未找到此文件:', source)
-#         return response
